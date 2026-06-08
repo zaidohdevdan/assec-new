0212a172b0bba367d78eaf38fb43ff6b2599684d
Zaidoh <danfealmeidafilho@gmail.com>
2026-06-08 20:49:14 -0300

    fix: make UsersModule global to resolve AuthGuard dependencies

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module, forwardRef, Global } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 
+@Global()
 @Module({
   imports: [PrismaModule, forwardRef(() => AuthModule)],
   controllers: [UsersController],
