c47ef85eb8857d57e34801785ed86a214ecf04a8
Zaidoh <danfealmeidafilho@gmail.com>
2026-06-08 20:15:10 -0300

    chore: add production domain to images remotePatterns

--- app/web/next.config.ts
+++ app/web/next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "assecce.com.br",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "picsum.photos",
         pathname: "/**",
       },
