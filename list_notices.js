20749642789239f4c7cdbbdc7bfc52cde6bac1e0
Zaidoh <danfealmeidafilho@gmail.com>
2026-06-16 10:49:11 -0300

    feat: add script to fetch and display notices from database

---
+++ list_notices.js
@@ -0,0 +1,5 @@
+const { PrismaClient } = require('@prisma/client');
+const p = new PrismaClient();
+p.notice.findMany({ select: { id: true, title: true, active: true } })
+  .then(r => { console.log(JSON.stringify(r, null, 2)); return p.$disconnect(); })
+  .catch(e => { console.error(e); process.exit(1); });
